bd48d9fc22f41382b177f831f8f82136cd6664dc
Pedro <pedroferreiracjr@gmail.com>
2021-10-23 16:37:20 -0300

    aula explicando os erros que podem acontecer quando utilizando módulos features e formulários

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { TemplateFormModule } from './template-form/template-form.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -6,18 +7,16 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TemplateFormComponent } from './template-form/template-form.component';
 import { DataFormComponent } from './data-form/data-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TemplateFormComponent,
     DataFormComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    TemplateFormModule,
     AppRoutingModule,
     NgbModule
   ],
