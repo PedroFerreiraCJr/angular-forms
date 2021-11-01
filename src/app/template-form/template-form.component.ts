a08546894c94515631f8dc6a2557f81949cc501c
Pedro <pedroferreiracjr@gmail.com>
2021-11-01 13:24:04 -0300

    aula sobre desenvolvimeno de chamada ajax para serviço ViaCEP

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, NgModel } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-template-form',
@@ -13,7 +14,9 @@ export class TemplateFormComponent implements OnInit {
     email: null
   };
 
-  constructor() { }
+  constructor(
+    private readonly http: HttpClient
+  ) { }
 
   ngOnInit(): void {
   }
@@ -37,5 +40,17 @@ export class TemplateFormComponent implements OnInit {
       'is-invalid': this.fieldInvalidAndTouched(field),
       'is-valid': this.fieldValidAndTouched(field)
     };
+  }
+
+  consultarCEP(event: any): void {
+    const value = event?.target?.value.replace(/\D/g, '');
+    if (value) {
+      const validarCEP = /^[0-9]{8}$/;
+      if (validarCEP.test(value)) {
+        this.http.get(`//viacep.com.br/ws/${value}/json`).subscribe((result) => {
+          console.log(result);
+        });
+      }
+    }
   }
 }
